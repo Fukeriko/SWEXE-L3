97c730bc2dd387748db1fbaae02506fe30a26311
Ubuntu <ubuntu@ip-172-31-57-19.ec2.internal>
2021-09-16 12:21:41 +0000

    NameError (undefined local variable or method `tweet' for #<TweetsController:0x000055f21128e020>

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,34 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = tweet.all
+  end
+  def new
+    @tweet = Tweet.new
+  end
+  def create
+    message = params[:tweet][:message]
+    tdate = params[:tweet][:tdate]
+    tweet = Tweet.new(message: message, tdate: tdate)
+    tweet.save
+    redirect '/'
+  end
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to '/'
+  end
+  def edit
+    @tweet =Tweet.find(params[:id])
+  end
+  def update
+    message = params[:tweet][:message]
+    tdate = params[:tweet][:tdate]
+    tweet = Tweet.find(params[:id])
+    tweet.update(message: message, tdate: tdate)
+    tweet.save
+    redirect_to '/'
   end
 end
